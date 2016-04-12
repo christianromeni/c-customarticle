8cdd86ce055c09fcd19b6dee6e3904e0c7440d78
Christian Romeni <c.romeni@brainfactory.de>
2016-04-12 16:16:21 +0200

    upps

--- src/christianromeni/Contao/CustomArticleBundle.php
+++ src/christianromeni/Contao/CustomArticleBundle.php
@@ -1,17 +1,6 @@
 <?php
-/*
- * Copyright MADE/YOUR/DAY OG <mail@madeyourday.net>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 namespace customarticle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-/**
- * Configures the RockSolid Frontend Helper bundle.
- *
- * @author Martin Auswöger <martin@madeyourday.net>
- */
 class CustomArticleBundle extends Bundle
 {
 }
